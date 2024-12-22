a6919a15ed6d47edab0e12bed590dc74f0aec460
loicDYlanrak <loicrakotoarivony07@gmail.com>
2024-12-22 22:40:47 +0300

    Add files via upload

---
+++ SGBD(vaovao)/src/Statement.java
@@ -0,0 +1,31 @@
+package jdbc;
+
+import base.DatabaseManager;
+
+public class Statement {
+    private DatabaseManager dbManager;
+
+    public Statement(DatabaseManager dbManager) {
+        this.dbManager = dbManager;
+    }
+
+    public ResultSet executeQuery(String sql) throws Exception {
+        String response = dbManager.executerRequete(sql);
+        return new ResultSet(response);
+    }
+
+    public int executeUpdate(String sql) throws Exception {
+        String response = dbManager.executerRequete(sql);
+        //return Integer.parseInt(response.split(" ")[0]); // Ex: "1 row(s) affected"
+        return 0;
+    }
+
+    public boolean execute(String sql) throws Exception {
+        String response = dbManager.executerRequete(sql);
+        return !response.isBlank();
+    }
+
+    public void close() {
+        // No resources to close in this simple implementation
+    }
+}
